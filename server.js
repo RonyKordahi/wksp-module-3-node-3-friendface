ae60227dde1c4158371e062fffc662bebac9ccf9
RonyKordahi <60363887+RonyKordahi@users.noreply.github.com>
2020-02-21 10:05:12 -0500

    Rony Kordahi - Commit
    Done?

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ const homepage = (req, res) => {
         return;
     }
     res.render("pages/homepage", {
+        user: currentUser,
         users: users,
-        title: `Welcome ${currentUser.name}!`,
+        title: `Welcome ${currentUser.name}! Here is your face!`,
         
     })
 }
@@ -45,19 +46,44 @@ const userID = (req, res) => {
         return;
     }
     const id = req.params.id;
-    res.send(`user ID is ${id}`);
+    const user = users.find(user => user.id === id)
+    res.render("pages/userID", {
+        user: user,
+        users: users,
+        title: `${user.name}'s face!`
+    })
+}
+
+const addFriend = (req, res) => {
+    const id = req.params.id;
+    currentUser.friends.push(id);
+    
+    res.redirect("/");
 }
 
 const getName = (req, res) => {
     const firstName = req.query.firstName;
     currentUser = users.find(user => user.name === firstName)
-
+    
     if(currentUser) {
         res.redirect("/");
     }
     else {
         res.redirect("/signin");
     }
+}
+
+const signOut = (req, res) => {
+    currentUser = null;
+    res.redirect("/");
+}
+
+const FFF = (req, res) => {
+    res.render("pages/FFF", {
+        currentUser: currentUser,
+        users: users,
+        title: "Find new faces! Click to add!"
+    })
 }
 
 // -----------------------------------------------------
@@ -73,6 +99,9 @@ express()
     .get("/signin", signin)
     .get("/user/:id", userID)
     .get("/getname", getName)
+    .get("/signout", signOut)
+    .get("/FFF", FFF)
+    .get("/add/:id", addFriend)
     .get('*', fourOhFour)
     
     .listen(PORT, () => console.log(`Listening on port ${PORT}`));
